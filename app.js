4c04f6e56fa673ccfc959b8ee4aa9fd6d1d2b457
M. Smith <smithryg@yahoo.com>
2019-05-22 19:08:39 -0400

    error handlers added

--- app.js
+++ app.js
@@ -56,6 +56,18 @@ app.post('/goodbye', (req, res) => {
     res.redirect('/hello');
 });
 
+app.use((req, res, next)=> {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error', err);
+});
+
 app.listen(3000, () => {
     console.log("Live on 3000")
 });
