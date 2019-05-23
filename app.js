9bfa33062394891b5927a1fef3a7036d33c8343e
M. Smith <smithryg@yahoo.com>
2019-05-23 17:50:43 -0400

    refactored code and added functionality

--- app.js
+++ app.js
@@ -4,59 +4,21 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 app.use(cookieParser());
 
 app.set('view engine', 'pug');
 
-app.use((req, res, next) => {
-    console.log('hello');
-    next();
-});
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
+
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
 
 app.use((req, res, next) => {
-    console.log('world');
-    next();
-});
-
-
-
-app.get('/', (req, res) => {
-    const name = req.cookies.username;
-    if (name) { 
-        res.render('index',{name})
-    } else {
-        res.redirect('/hello')
-    }
-});
-
-app.get('/cards', (req, res) => {
-    res.render('card', {
-        prompt: "Who got shot, and still gave a speech",
-    });
-});
-
-app.get('/hello', (req, res) => {
-    const name = req.cookies.username;
-    if (name){ 
-        res.redirect('/');
-    }else {
-        res.render('hello');
-    }
-});
-
-
-app.post('/hello', (req, res) => {
-    res.cookie('username', req.body.username);
-    res.redirect('/');
-});
-
-app.post('/goodbye', (req, res) => {
-    res.clearCookie('username');
-    res.redirect('/hello');
-});
-
-app.use((req, res, next)=> {
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
